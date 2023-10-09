8ce8d48f61bc9c5d0d4835f704b163f76d436ac0
Edward Johnston <ejohnston86@gmail.com>
2023-10-09 15:46:28 -0400

    WIP - update unit tests

--- src/tests/unit/rest_api/test_handler.py
+++ src/tests/unit/rest_api/test_handler.py
@@ -1,56 +1,89 @@
 import json
 import pytest
-from unittest.mock import Mock
+import pandas as pd
+import numpy as np
+from unittest.mock import Mock, patch
+from src.main.framework.models import ResponseModel
 from src.main.rest_api.handler import (
     get_response,
     call_api,
     panda_to_json,
     numpy_to_list,
     get_closing_summary,
-    prediction_handler
+    prediction_handler,
 )
 
 
-# Mock the requests module for testing
-@pytest.fixture(autouse=True)
-def mock_requests_get(monkeypatch):
-    def mock_get(*args, **kwargs):
-        class MockResponse:
-            def __init__(self, content, status_code):
-                self.content = content
-                self.status_code = status_code
+class TestHandler:
 
-            def json(self):
-                return json.loads(self.content)
+    @pytest.fixture
+    def mock_response(self):
+        response = Mock()
+        response.status_code = 200
+        response.json.return_value = {"example_key": "example_value"}
+        return response
 
-        return MockResponse("{'data': 'sample data'}", 200)
+    @patch("requests.get")
+    def test_get_response(self, mock_get, mock_response):
+        mock_get.return_value = mock_response
+        result = get_response(url="http://example.com")
+        assert result.status_code == 200
 
-    monkeypatch.setattr("requests.get", mock_get)
+    @pytest.fixture
+    def mock_session_handler(self):
+        session_handler = Mock()
+        session_handler.config = {
+            "alphavantage_url": "http://example.com",
+            "alphavantage_api_key": "your_api_key",
+        }
+        session_handler.req_body.query_params.symbol = "AAPL"
+        return session_handler
 
-@pytest.fixture(autouse=True)
-def mock_response(mock_get):
+    @patch("requests.get")
+    def test_call_api(self, mock_get, mock_response, mock_session_handler):
+        mock_get.return_value = mock_response
+        result = call_api(session_handler=mock_session_handler)
+        assert result.status_code == 200
 
+    def test_panda_to_json(self):
+        df = {"column1": [1, 2, 3], "column2": ["a", "b", "c"]}
+        result = panda_to_json(pd.DataFrame(df))
+        assert result == [{"column1": 1, "column2": "a"}, {"column1": 2, "column2": "b"}, {"column1": 3, "column2": "c"}]
 
-# Write test functions for each function in handler.py
-def test_get_response():
-    # Your test cases for get_response function
+    def test_numpy_to_list(self):
+        np_array = np.array([1, 2, 3])
+        result = numpy_to_list(np_array)
+        assert result == [1, 2, 3]
 
+    def test_get_closing_summary(self):
+        predicted_close = [10.5, 12.3, 9.8, 11.2]
+        result = get_closing_summary(predicted_close)
+        assert result == {"min_close": 9.8, "mean_close": 11.2, "max_close": 12.3}
 
-def test_call_api():
-    # Your test cases for call_api function
+    @patch("src.main.rest_api.handler.call_api")
+    @patch("src.main.rest_api.handler.assemble_data")
+    @patch("src.main.rest_api.handler.get_last_10_data_points")
+    @patch("src.main.rest_api.handler.StockPricePredictor")
+    def test_prediction_handler(
+        self, mock_stock_predictor, mock_last_10_data_points, mock_assemble_data, mock_call_api
+    ):
+        # Mock the necessary dependencies and data
+        session_handler = Mock()
+        session_handler.state.config = Mock()
+        mock_call_api.return_value = Mock()
+        mock_assemble_data.return_value = (Mock(), Mock())
+        mock_last_10_data_points.return_value = Mock()
+        mock_stock_predictor_instance = mock_stock_predictor.return_value
+        mock_stock_predictor_instance.predict_random_forest.return_value = [10.5, 12.3, 9.8, 11.2]
+        mock_stock_predictor_instance.predict_linear_regression.return_value = [9.7, 11.1, 10.0, 12.0]
 
+        # Call the prediction_handler function
+        result = prediction_handler(session_handler=session_handler)
 
-def test_panda_to_json():
-    # Your test cases for panda_to_json function
-
-
-def test_numpy_to_list():
-    # Your test cases for numpy_to_list function
-
-
-def test_get_closing_summary():
-    # Your test cases for get_closing_summary function
-
-
-def test_prediction_handler():
-    # Your test cases for prediction_handler function
+        # Assert the expected ResponseModel
+        expected_response = ResponseModel(
+            predicted_close_rf={"min_close": 9.8, "mean_close": 11.2, "max_close": 12.3},
+            predicted_close_lr={"min_close": 9.7, "mean_close": 10.95, "max_close": 12.0},
+            demark_data=Mock(),
+        )
+        assert result == expected_response
--- src/tests/unit/test_application.py
+++ src/tests/unit/test_application.py
@@ -1,33 +1,41 @@
 import os
 from fastapi.testclient import TestClient
 from src.main.application import app
+from src.main.framework.models import ResponseModel
+from src.main.rest_api.handler import prediction_handler
+from fastapi import Request
+from src.main.middleware import RequestMiddleware
+import pytest
 
-# Create a TestClient instance
+# Create a TestClient instance for testing
 client = TestClient(app)
+
+# Mocked SessionHandler class
+class MockedSessionHandler:
+    def __init__(self, config, req_body, app):
+        pass
+
+# Mocked prediction_handler function
+async def mocked_prediction_handler(session_handler):
+    return ResponseModel(predicted_close_rf={}, predicted_close_lr={}, demark_data={})
+
+# Mock the SessionHandler class and prediction_handler function
+app.dependency_overrides[RequestMiddleware] = MockedSessionHandler
+app.dependency_overrides[prediction_handler] = mocked_prediction_handler
 
 def test_get_health():
     response = client.get("/health")
     assert response.status_code == 200
-    assert response.json() == {
-        "message": "Hello from the other side.",
-        "version": "0.0.0"
-    }
+    assert response.json() == {"message": "Hello from the other side.", "version": "0.0.0"}
 
-def test_predict_stock_price(mock_app_config, mock_app):
-    # You can write similar tests for the /predict endpoint
-    query_params = {
-        "symbol": "AAPL",
-        "start_date": "09-10-2023"
-    }
-    headers = {
-        "trackingId": "12345"
-    }
-    response = client.get(
-        "/predict",
-        params=query_params,
-        headers=headers
-    )
+@pytest.fixture
+def mocked_request():
+    request = Request(scope=None, receive=None)
+    return request
+
+def test_predict_stock_price(mocked_request):
+    response = client.get("/predict", request=mocked_request)
     assert response.status_code == 200
-    # Add more assertions based on the expected response data
+    assert response.json() == {"predicted_close_rf": {}, "predicted_close_lr": {}, "demark_data": {}}
 
-# Add more test functions for other endpoints as needed
+# You can add more test cases for other routes as needed.
